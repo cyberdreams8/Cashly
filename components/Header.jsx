64476432abd4d70933c8922a0749928c1a9b2146
cyberdreams8 <nethra.nayak23@spit.ac.in>
2025-07-29 01:04:18 +0530

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image"
+
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Button } from './ui/button';
+import { LayoutDashboard, PenBox } from 'lucide-react';
+
+
+const Header = () => {
+  return (
+    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b '>
+      <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
+        
+        <Link href="/">
+          <Image src={"/logo.png"} alt="welth logo" height={60} width={200} className="h-12 w-auto object-contain"/>
+        </Link>
+      
+
+        <div className='flex items-center space-x-4'> 
+          <SignedIn>
+            <Link href={"/dashboard"} className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
+              <Button variant="outline">
+                <LayoutDashboard size={18}/>
+                <span className='hidden md:inline'>Dashboard</span>
+              </Button>
+            </Link>
+
+            <Link href={"/transaction/create"} className='flex items-center gap-2'>
+              <Button>
+                <PenBox size={18}/>
+                <span className='hidden md:inline'>Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
+          <SignedOut forceRedirectUrl="/dashboard">
+            <SignInButton>
+            <Button variant="outline">Login</Button>            
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton appearance={{
+              elements:{
+                avatarBox: "w-10 h-10"
+              }
+            }}/>
+          </SignedIn>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header;
